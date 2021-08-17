4ca915d856abe5e65b8078ac9bec6102d30bd241
Jonathan Deak <jonathan@legendarysystems.io>
2021-08-17 18:03:34 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+#copyright Jonathan David Deak
+#code may be used for non comercial uses.
+Any commercial use shall pay 10% of all earnings befor taxes or any deduction to Jonathan David Deak in royalties.
+
+
+class law(self, state)
+    def __init__ self.state = state
+    def self.courtOpinions = courtOpinions[]
+
+
+class courtOpinion()
+    def self.court = court
+
+class court()
+    location
+    superiorCourt
+    inferiorCourt
+
+class subjectMatterExpert
+class state
+class user
+    userState = plantiff #make type bool
+
+class problemSolver
+
+
+
+
+problems
+
+
+class userProgress
+    previousMotions
+    pendingMotions
+    possibleMotions
+
+class motion()
+    modified
+    petition
+    dissmiss
+    useCase []
+    supportingEvidence
+    brief()
+
+    
+
+
