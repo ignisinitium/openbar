534372db1e045a916b4047bfcccd14efa4d767d1
Ignis Initium <jonathan@legendarysystems.io>
2021-08-19 19:11:27 -0400

    tested creating and viewing tables

--- webapp/database.py
+++ webapp/database.py
@@ -3,12 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="tearsfor_openBar",
-  password="yourpassword"
+  password="4S}b]L6B-~b(",
+  database="tearsfor_openBar"
+
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SHOW DATABASES")
 
+#mycursor.execute("CREATE TABLE users (firstName VARCHAR(255),lastName VARCHAR(255), streetAddress VARCHAR(255), state VARCHAR(255), $
+mycursor.execute("SHOW TABLES")
 for x in mycursor:
   print(x)
