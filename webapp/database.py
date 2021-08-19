ee63058c70871541064fb48b6df90963407bf4eb
Ignis Initium <jonathan@legendarysystems.io>
2021-08-19 19:28:06 -0400

    tested creating and viewing tables. It works!

--- webapp/database.py
+++ webapp/database.py
@@ -5,13 +5,14 @@ mydb = mysql.connector.connect(
   user="tearsfor_openBar",
   password="4S}b]L6B-~b(",
   database="tearsfor_openBar"
-
 )
 
 mycursor = mydb.cursor()
 
+#mycursor.execute("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, firstName VARCHAR(255),lastName VARCHAR(255), streetAddress VARCHAR(255), state VARCHAR(255), $
 
-#mycursor.execute("CREATE TABLE users (firstName VARCHAR(255),lastName VARCHAR(255), streetAddress VARCHAR(255), state VARCHAR(255), $
 mycursor.execute("SHOW TABLES")
-for x in mycursor:
+  for x in mycursor:
   print(x)
+
+
