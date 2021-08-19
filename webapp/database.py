3c5413be41905fefa75d34281981e2c706a1b052
ignisinitium <ignisinitium@icloud.com>
2021-08-19 17:17:00 -0400

    updated functions

--- webapp/database.py
+++ webapp/database.py
@@ -1,37 +1,14 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='tearsfor_openBar', password='', host='127.0.0.1', database='tearsfor_openBar')
-cnx.close()
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="tearsfor_openBar",
+  password="yourpassword"
+)
 
-from __future__ import print_function
+mycursor = mydb.cursor()
 
-import mysql.connector
-from mysql.connector import errorcode
+mycursor.execute("SHOW DATABASES")
 
-DB_NAME = 'tearsfor_openBar'
-
-TABLES = {}
-TABLES['tearsfor_openBar'] = (
-    "CREATE TABLE `users` ("
-    "  `user_no` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `birth_date` date NOT NULL,"
-    "  `first_name` varchar(14) NOT NULL,"
-    "  `last_name` varchar(16) NOT NULL,"
-    "  `gender` enum('M','F') NOT NULL,"
-      "  PRIMARY KEY (`user_no`)"
-    ") ENGINE=InnoDB")
-
-query = ("SELECT first_name, last_name, FROM users "
-         "WHERE birth_date BETWEEN %s AND %s")
-
-birth_start = datetime.date(1999, 1, 1)
-birth_end = datetime.date(1999, 12, 31)
-
-cursor.execute(query, (birth_start, birth_end))
-
-for (first_name, last_name, birth_date) in cursor:
-  print("{}, {} was born on {:%d %b %Y}".format(
-    last_name, first_name, birth_date))
-
-cursor.close()
-cnx.close()
+for x in mycursor:
+  print(x)
