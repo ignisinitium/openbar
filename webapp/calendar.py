03875b5814248f6fe85352e3c709a1bcb2a07e2a
Ignis Initium <jonathan@legendarysystems.io>
2021-08-27 22:20:20 -0400

    Added Questionare and calendar

---
+++ webapp/calendar.py
@@ -0,0 +1,4 @@
+#for each state know the rules that pertain to court porceedrue for the
+# number of days requiaared for any notice or last day to file ann appeal ect...
+#send text alerts of upcomming events
+
